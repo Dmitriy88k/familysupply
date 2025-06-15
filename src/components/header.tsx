dfbf6d88aeb889efaaf5db1fda28ad2de954cc40
Dmitriy Khegay <dmitriy88k@gmail.com>
2025-06-15 14:05:30 -0400

    added a home page, set react- router DOM

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo2.png"
 import { Link } from "react-router-dom";
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -23,7 +23,7 @@ const Header = () => {
         <div style={{backgroundColor: "#212529"}} className="h-[108px]  flex items-center relative">
             <div className="w-[90%] mx-auto flex items-center justify-between md:justify-start">
                 <div>
-                    <img src={Logo} alt="" className="w-19"/>
+                    <Link to="/"><img src={Logo} alt="" className="w-19 cursor-pointer hover:scale-105 transition-transform duration-300"/></Link>
                 </div>
 
                 <ul className="hidden md:flex gap-6 text-white text-sm ml-25">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,18 @@
+import HomeImg from "../assets/home.jpg"
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 const Home = () => {
     return (
-        <div>
-            <h1>This is a Home page</h1>
-        </div>
+        <motion.div initial={{ opacity: -2, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 2}} className="flex flex-col gap-10 items-center md:w-[90%] mx-auto lg:flex-row md:my-18">
+            <img src={HomeImg} alt="" className="lg:w-[60%]"/>
+            <div className="w-[90%] mx-auto md:py-17">
+                <h1 className="text-5xl mb-10 ">Your Trusted Source for Home Cleaning Supplies</h1>
+                <p className="mb-10  text-sm">At Family Supply, we are dedicated to providing high-quality cleaning products that make your home shine. Our mission is to simplify your cleaning routine with reliable supplies delivered right to you</p>
+                <Link to="/products"><button style={{background: "#FF6F34"}} className="px-6 py-2 rounded-4xl text-amber-50 font-semibold mr-3 cursor-pointer hover:scale-110 transition-transform duration-300" >Shop</button></Link>
+                <Link to="/about"><button style={{background: "#212529"}} className="px-6 py-2 rounded-4xl font-semibold text-amber-50 cursor-pointer hover:scale-110 transition-transform duration-300" >Learn More</button></Link>
+            </div>
+        </motion.div>
     )
 }
 
